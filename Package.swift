d8b194265e0c49630242e83b3988307a85ecf0c8
Andrew Thompson <mrwerdo331@me.com>
2016-06-11 16:53:33 +1000

    Seperated LibTIFF from CLibTIFF
    The SwiftPM was throwing an error because clang modules cannot have
    swift dependencies.

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "LibTIFF",
+    targets: [ // ]
+        Target(name: "LibTIFF")
+    ],
+    dependencies: [ // ]
+        .Package(url: "https://github.com/mrwerdo/CLibTIFF", 
+                 majorVersion: 0,
+                 minor: 1),
+        .Package(url: "https://github.com/mrwerdo/Geometry",
+                 majorVersion: 1)
+    ]
+)
+
