c9b42e96909db66ab35ff6fb2c4b957b4991ca7a
Andrew Thompson <mrwerdo331@me.com>
2016-06-18 13:29:33 +1000

    Image testing is put into a tempory directory.

--- Tests/LibTIFF/LibTIFF.swift
+++ Tests/LibTIFF/LibTIFF.swift
@@ -4,20 +4,47 @@ import CLibTIFF
 @testable import LibTIFF
 
 class TIFFFileTests : XCTestCase {
+
+    var basePath: String!
+    var tempPath: String!
+
+    func path(function: String = #function) -> String {
+        let path = "\(basePath!)\(function).tiff"
+        return path
+    }
+
+    override func setUp() {
+        let template = "/tmp/tmpdir.XXXXXX"
+        template.withCString { (cstr) in 
+            if let cpath = mkdtemp(UnsafeMutablePointer(cstr)) {
+                let path = String(cString: cpath)
+                basePath = path + "/"
+                tempPath = path
+            } else {
+                exit(EXIT_FAILURE)
+            }
+        }
+    }
+
+    override func tearDown() {
+        if unlink(tempPath) != -1 {
+            exit(EXIT_FAILURE)
+        }
+    }
+
     func testWritingAndReading() {
         let size = 100 * 100 * 3
         var written = [UInt8](repeating: 0, count: size)
-        let path = "/Users/mrwerdo/Desktop/test.tiff"
-        let image = try! TIFFFile(writingAt: path, size: Size(100, 100), hasAlpha: false)
-        
-        for y in 0..<100 {
-            for dx in 0..<100 {
-                let x = dx * 4
-                image.buffer[y * 100 + x + 0] = 255
-                image.buffer[y * 100 + x + 1] = 0
-                image.buffer[y * 100 + x + 2] = 0
-            }
+        let image = try! TIFFFile(writingAt: path(), size: Size(100, 100), hasAlpha: false)
+
+        // Turn on every red pixel.
+        var c = 0
+        while c < size {
+            let v: UInt8 = c % 3 == 0 ? 255 : 0
+            image.buffer[c] = v
+            c += 1
         }
+
         for i in 0..<size {
             written[i] = image.buffer[i]
         }
@@ -25,7 +52,59 @@ class TIFFFileTests : XCTestCase {
         try! image.write()
         image.close()
 
-        let reading = try! TIFFFile(readingAt: path)
+        let reading = try! TIFFFile(readingAt: path())
+        for i in 0..<size {
+            XCTAssert(written[i] == reading.buffer[i], "contents of written file != contents of read file")
+        }
+    }
+
+    func testBlueAndGreenImageVertical() {
+        let size = 100 * 100 * 3
+        var written = [UInt8](repeating: 0, count: size)
+        let image = try! TIFFFile(writingAt: path(), size: Size(100, 100), hasAlpha: false)
+
+        for y in 0..<100 {
+            for x in 0..<100 {
+                let offset = x % 3
+                let index = y * 300 + 3 * x + offset
+                image.buffer[index] = 255
+            }
+        }
+
+        for i in 0..<size {
+            written[i] = image.buffer[i]
+        }
+
+        try! image.write()
+        image.close()
+
+        let reading = try! TIFFFile(readingAt: path())
+        for i in 0..<size {
+            XCTAssert(written[i] == reading.buffer[i], "contents of written file != contents of read file")
+        }
+    }
+
+    func testBlueAndGreenImageHorizontal() {
+        let size = 100 * 100 * 3
+        var written = [UInt8](repeating: 0, count: size)
+        let image = try! TIFFFile(writingAt: path(), size: Size(100, 100), hasAlpha: false)
+
+        for y in 0..<100 {
+            let offset = y % 3
+            for x in 0..<100 {
+                let index = y * 300 + 3 * x + offset
+                image.buffer[index] = 255
+            }
+        }
+
+        for i in 0..<size {
+            written[i] = image.buffer[i]
+        }
+
+        try! image.write()
+        image.close()
+
+        let reading = try! TIFFFile(readingAt: path())
         for i in 0..<size {
             XCTAssert(written[i] == reading.buffer[i], "contents of written file != contents of read file")
         }
@@ -34,6 +113,8 @@ class TIFFFileTests : XCTestCase {
     static var allTests : [(String, (TIFFFileTests) -> () throws -> Void)] {
         return [
             ("testWritingAndReading", testWritingAndReading),
+            ("testBlueAndGreenVertical", testBlueAndGreenImageVertical),
+            ("testBlueAndGreenImageHorizontal", testBlueAndGreenImageHorizontal),
         ]
     }
 }
