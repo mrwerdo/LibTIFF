96871a6107ef917c111aad0ebf2ff4b0a02c5923
Andrew Thompson <mrwerdo331@me.com>
2016-06-18 21:55:26 +1000

    Created ImageProtocol, which defines common traits for images.

---
+++ Sources/Image.swift
@@ -0,0 +1,89 @@
+// -----------------------------------------------------------------------------
+// Image.swift
+// -----------------------------------------------------------------------------
+
+import Geometry
+
+/// Protocol which all images should conform to.
+/// 
+/// When working with images, we generally want:
+/// 
+///  - width
+///  - height
+///  - access to the pixels
+///  - want that access to be easy
+///  - what to initalise it with a size?
+///  - what to control the data type of each individual color channel (i.e. 
+///     64 bits per color channel)
+///  - size of channels ( basically what we were saying before about 64 bits)
+///  - number of channels
+/// 
+
+public struct Pixel<Channel> {
+    var count: Int
+    var channels: UnsafeMutablePointer<Channel>
+    var index: Int
+
+    public init(channels: UnsafeMutablePointer<Channel>, count: Int, index: Int) {
+        self.count = count
+        self.channels = channels
+        self.index = index
+    }
+}
+
+public protocol ImageProtocol : Collection {
+    associatedtype Channel
+
+    var size: Size { get set }
+    var channelCount: Int { get }
+    var buffer: UnsafeMutablePointer<Channel> { get }
+}
+
+extension ImageProtocol {
+    public subscript(i: Int) -> Pixel<Channel> {
+        let index = i * channelCount
+        return Pixel(channels: buffer.advanced(by: index), 
+                     count: channelCount, 
+                     index: index)
+    }
+}
+
+extension ImageProtocol {
+    public var startIndex: Int {
+        return 0
+    }
+    public var endIndex: Int {
+        return size.width * size.height
+    }
+    public var underestimatedCount: Int {
+        return endIndex
+    }
+    public func index(after i: Int) -> Int {
+        return i + 1
+    }
+}
+
+struct ImageIterator<Image: ImageProtocol> : IteratorProtocol {
+    var index: Int
+    var image: Image
+    
+    mutating func next() -> Pixel<Image.Channel>? {
+        let next = index
+        guard next != image.endIndex else {
+            return nil
+        }
+        index += 1
+        return image[next]
+    }
+    
+    init(_ image: Image) {
+        self.image = image
+        self.index = 0
+    }
+}
+
+extension ImageProtocol {
+//    func makeIterator() -> ImageIterator<Self> {
+//        return ImageIterator(self)
+//    }
+}
--- Sources/TIFFImage.swift
+++ Sources/TIFFImage.swift
@@ -1,175 +1,18 @@
 import Geometry
 import CLibTIFF
 
-public class TIFFImage {
-
-	public var tiffref: OpaquePointer // TIFF* in c
-	public var path: String
-
-	public var size: Size
-	/// Buffer must be stored like so:
-	/// when	y=0, x1, x2, x3, x4, x5, ..., xN
-	/// when	y=1, x1, x2, x3, x4, x5, ..., xN
-	/// when	y=..., ...,
-	/// when	y=N, x1, x2, x3, x4, x5, ..., xN
-	public var buffer: UnsafeMutablePointer<UInt8>
-	/// Set this to `true` to have the buffer detroyed when the object goes out of scope.
-	public var ownsBuffer: Bool = false
-	private(set) var hasAlpha: Bool
-
-	private var bitsPerSample: UInt32
-	private var samplesPerPixel: UInt32
-	private var rowsPerStrip: UInt32 = 1
-	private var extraChannels: [UInt16]
-	private var photometric: UInt32
-	private var planarconfig: UInt32
-	private var orientation: UInt32
-
-	public enum Errors : ErrorProtocol {
-		case Open
-		case Flush
-		case WriteScanline
-		case SetField
-		case GetField
-	}
-
-
-    public init(readAt path: String) throws {
-        guard let ptr = TIFFOpen(path, "r") else {
-            throw Errors.Open
-        }
-        tiffref = ptr
-        self.size = Size(0,0)
-        self.hasAlpha = false
-        self.buffer = UnsafeMutablePointer<UInt8>(allocatingCapacity: 0)
-        self.path = path
-        self.bitsPerSample = 0
-        self.samplesPerPixel = 0
-        self.rowsPerStrip = 0
-        self.extraChannels = []
-        self.photometric = 0
-        self.planarconfig = 0
-        self.orientation = 0
-
-        try readFields()
+public class TIFFImage : ImageProtocol {
+    public var size: Size
+    public private(set) var buffer: UnsafeMutablePointer<UInt32>
+    public var channelCount: Int {
+        return hasAlpha ? 4 : 3
     }
+    public private(set) var hasAlpha: Bool
 
-
-	public init(writeAt path: String, _ buffer: UnsafeMutablePointer<UInt8>, _ size: Size, hasAlpha: Bool) throws {
-		guard let ptr = TIFFOpen(path, "w") else {
-			throw Errors.Open
-		}
-		tiffref = ptr
-		self.size			= size
-		self.hasAlpha		= hasAlpha
-		self.buffer			= buffer
-		self.path			= path
-		bitsPerSample		= 8
-		samplesPerPixel		= 3 + (hasAlpha ? 1 : 0)
-		rowsPerStrip		= 1
-		photometric			= UInt32(PHOTOMETRIC_RGB)
-		planarconfig		= UInt32(PLANARCONFIG_CONTIG)
-		orientation			= UInt32(ORIENTATION_TOPLEFT)
-		if hasAlpha {
-			extraChannels	= [UInt16(EXTRASAMPLE_ASSOCALPHA)]
-		} else {
-			extraChannels	= []
-		}
-		try writeFields()
-	}
-	
-	public func flush() throws {
-		guard TIFFFlush(tiffref) == 1 else {
-			throw Errors.Flush
-		}
-	}
-	
-	/// Call this explicitly if the object is in global scope, otherwise pending writes may not be written.
-	/// Alternatively, use `flush()`.
-	public func close() {
-		TIFFClose(tiffref)
-	}
-
-	deinit {
-		TIFFClose(tiffref)
-		if ownsBuffer {
-			buffer.deallocateCapacity(size.width * size.height * Int(samplesPerPixel))
-		}
-	}
-
-	public func write() throws {
-		for y in 0..<size.height {
-			guard TIFFWriteScanline(tiffref, buffer.advanced(by: y * size.width * Int(samplesPerPixel)), UInt32(y), 0) == 1 else {
-				throw Errors.WriteScanline
-			}
-		}
-		try flush()
-	}
-
-	private func setField(_ tag: Int32, _ value: UInt32) throws {
-		guard TIFFSetField_uint32(tiffref, UInt32(tag), value) == 1 else {
-			throw Errors.SetField
-		}
-	}
-
-	private func setSize(_ size: Size) throws {
-		let width = UInt32(size.width)
-		let height = UInt32(size.height)
-		try setField(TIFFTAG_IMAGEWIDTH, width)
-		try setField(TIFFTAG_IMAGELENGTH, height)
-	}
-
-	private func writeFields() throws {
-		try setSize(size)
-		try setField(TIFFTAG_BITSPERSAMPLE, bitsPerSample)
-		try setField(TIFFTAG_SAMPLESPERPIXEL, samplesPerPixel)
-		try setField(TIFFTAG_ROWSPERSTRIP, rowsPerStrip)
-		if extraChannels.count > 0 {
-			guard TIFFSetField_ExtraSample(tiffref, UInt16(extraChannels.count), &extraChannels) == 1 else {
-				throw Errors.SetField
-			}
-		}
-		try setField(TIFFTAG_PHOTOMETRIC, photometric)
-		try setField(TIFFTAG_PLANARCONFIG, planarconfig)
-		try setField(TIFFTAG_ORIENTATION, orientation)
-	}
-
-	private func getField(_ tag: Int32) throws -> UInt32 {
-		var value: UInt32 = 0
-		guard TIFFGetField_uint32(tiffref, UInt32(tag), &value) == 1 else {
-			throw Errors.GetField
-		}
-		return value
-	}
-
-	private func getSize() throws -> Size {
-		let width = try getField(TIFFTAG_IMAGEWIDTH)
-		let height = try getField(TIFFTAG_IMAGELENGTH)
-		return Size(Int(width), Int(height))
-	}
-
-	private func readFields() throws {
-		size = try getSize()
-		bitsPerSample = try getField(TIFFTAG_BITSPERSAMPLE)
-		samplesPerPixel = try getField(TIFFTAG_SAMPLESPERPIXEL)
-		rowsPerStrip = try getField(TIFFTAG_ROWSPERSTRIP)
-		// TODO: Implement a function to get a C array - for extraChannels
-
-        var count: UInt16 = 4
-        var channels: UnsafeMutablePointer<UInt16>? = UnsafeMutablePointer<UInt16>(allocatingCapacity: Int(count))
-        guard TIFFGetField_ExtraSample(tiffref, &count, &channels) == 1 else {
-            throw Errors.GetField
-        }
-        if let channels = channels {
-            for index in 0..<Int(count) {
-                extraChannels[index] = channels[index]
-            }
-        } else {
-            throw Errors.GetField
-        }
-
-		photometric = try getField(TIFFTAG_PHOTOMETRIC)
-		planarconfig = try getField(TIFFTAG_PLANARCONFIG)
-		orientation = try getField(TIFFTAG_ORIENTATION)
-	}
+    public init(size: Size, hasAlpha: Bool = false) {
+        self.hasAlpha = true
+        self.size = size
+        let c = size.width * size.height * (hasAlpha ? 4 : 3)
+        self.buffer = UnsafeMutablePointer(allocatingCapacity: c)
+    }
 }
