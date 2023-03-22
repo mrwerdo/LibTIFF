f537549e353bbad6221c721fe3493c66e023e41f
Michael Dales <michael@digitalflapjack.com>
2023-03-22 16:32:54 +0000

    Update to libtiff 4.5.0

--- Sources/CLibTIFF/SwiftWrapper.c
+++ Sources/CLibTIFF/SwiftWrapper.c
@@ -3,26 +3,26 @@
 #include "include/tiffio.h"
 #include "include/tiffvers.h"
 
-int TIFFGetField_uint32(TIFF *tif, ttag_t tag, uint32 *v) {
+int TIFFGetField_uint32(TIFF *tif, ttag_t tag, uint32_t *v) {
 	return TIFFGetField(tif, tag, v);
 }
 
-int TIFFGetField_uint16(TIFF *tif, ttag_t tag, uint16 *v) {
+int TIFFGetField_uint16(TIFF *tif, ttag_t tag, uint16_t *v) {
 	return TIFFGetField(tif, tag, v);
 }
 
-int TIFFSetField_uint32(TIFF *tif, ttag_t tag, uint32 v) {
+int TIFFSetField_uint32(TIFF *tif, ttag_t tag, uint32_t v) {
 	return TIFFSetField(tif, tag, v);
 }
 
-int TIFFSetField_uint16(TIFF *tif, ttag_t tag, uint16 v) {
+int TIFFSetField_uint16(TIFF *tif, ttag_t tag, uint16_t v) {
 	return TIFFSetField(tif, tag, v);
 }
 
-int TIFFSetField_ExtraSample(TIFF *tif, uint16 count, uint16 *types) {
+int TIFFSetField_ExtraSample(TIFF *tif, uint16_t count, uint16_t *types) {
 	return TIFFSetField(tif, TIFFTAG_EXTRASAMPLES, count, types);
 }
 
-int TIFFGetField_ExtraSample(TIFF *tif, uint16 *count, uint16* types[]) {
+int TIFFGetField_ExtraSample(TIFF *tif, uint16_t *count, uint16_t* types[]) {
     return TIFFGetField(tif, TIFFTAG_EXTRASAMPLES, count, types);
 }
--- Sources/CLibTIFF/include/SwiftWrapper.h
+++ Sources/CLibTIFF/include/SwiftWrapper.h
@@ -6,20 +6,20 @@
 /*
  * Functions to get a field.
  */
-int TIFFGetField_uint32(TIFF *tif, ttag_t tag, uint32 *v);
-int TIFFGetField_uint16(TIFF *tif, ttag_t tag, uint16 *v);
+int TIFFGetField_uint32(TIFF *tif, ttag_t tag, uint32_t *v);
+int TIFFGetField_uint16(TIFF *tif, ttag_t tag, uint16_t *v);
 
 /*
  * Functions to set a field.
  */
-int TIFFSetField_uint32(TIFF *tif, ttag_t tag, uint32 v);
-int TIFFSetField_uint16(TIFF *tif, ttag_t tag, uint16 v);
+int TIFFSetField_uint32(TIFF *tif, ttag_t tag, uint32_t v);
+int TIFFSetField_uint16(TIFF *tif, ttag_t tag, uint16_t v);
 
 /*
  * Sets & gets the extra sample field.
  * This is special, as it removes boilerplate from the swift code.
  */
-int TIFFSetField_ExtraSample(TIFF *tif, uint16 count, uint16 *types);
-int TIFFGetField_ExtraSample(TIFF *tif, uint16 *count, uint16 *types[]);
+int TIFFSetField_ExtraSample(TIFF *tif, uint16_t count, uint16_t *types);
+int TIFFGetField_ExtraSample(TIFF *tif, uint16_t *count, uint16_t *types[]);
 
 #endif /* _SWIFT_WRAPPER_ */
